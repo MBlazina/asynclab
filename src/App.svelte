c4d61bab4541df2e9c739aeeabe16eaa23389408
MBlazina <mirko.blazina@gmail.com>
2020-10-10 06:57:58 +0200

    fix app.svelte error

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,34 @@
 <script>
-	export let name;
+  export let name;
 </script>
-
-<main>
-	<h1>Helloooo {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <!--ADD lang="scss", variables "$primary" from rollup.config scss({prependData})-->
 <style lang="scss">
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-		h1 {
-		color:$primary;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+    h1 {
+      color: $primary;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+  }
 
-	
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Helloooo {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
